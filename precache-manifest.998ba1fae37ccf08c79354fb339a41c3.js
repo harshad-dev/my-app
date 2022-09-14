c3e437f48507bf4ff2ce33b31ba7447383278b01
Harshad Khadse <harshadkhadse1992@gmail.com>
2022-09-14 12:19:33 +0530

    Updates

---
+++ precache-manifest.998ba1fae37ccf08c79354fb339a41c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a79c1abb7dce84663f8",
+    "url": "/my-app/static/css/main.1fa74935.chunk.css"
+  },
+  {
+    "revision": "9a79c1abb7dce84663f8",
+    "url": "/my-app/static/js/main.9a79c1ab.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/my-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/my-app/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "ad6099f2e84041bd8e59",
+    "url": "/my-app/static/js/runtime~main.ad6099f2.js"
+  },
+  {
+    "revision": "45d628048762e0728107ae73ceb24c45",
+    "url": "/my-app/index.html"
+  }
+];
